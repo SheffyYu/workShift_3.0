39c9967a5b65d6f15e21573413d16b961b485831
sheffyYu <ysh5835938163@163.com>
2018-11-29 11:01:44 +0800

    2018.11.29 y:创建角色&菜单表，以及三表之间的关联表

---
+++ src/main/java/com/gzport/meeting/domain/entity/RoleMenu.java
@@ -0,0 +1,76 @@
+package com.gzport.meeting.domain.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by yushihui on 2018/11/29.
+ */
+@Entity
+@Table(name = "ROLE_MENU")
+public class RoleMenu {
+    @Id
+    @Column(name = "ROLE_ID")
+    private String roleId;
+    @Column(name = "MENU_ID")
+    private String menuId;
+
+    @Column(name = "INS_ACCOUNT")
+    private String insAccount;
+    @Column(name = "INS_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date insTimestamp;
+    @Column(name = "UPD_ACCOUNT")
+    private String updAccount;
+    @Column(name = "UPD_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updTimestamp;
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(String menuId) {
+        this.menuId = menuId;
+    }
+
+    public String getInsAccount() {
+        return insAccount;
+    }
+
+    public void setInsAccount(String insAccount) {
+        this.insAccount = insAccount;
+    }
+
+    public Date getInsTimestamp() {
+        return insTimestamp;
+    }
+
+    public void setInsTimestamp(Date insTimestamp) {
+        this.insTimestamp = insTimestamp;
+    }
+
+    public String getUpdAccount() {
+        return updAccount;
+    }
+
+    public void setUpdAccount(String updAccount) {
+        this.updAccount = updAccount;
+    }
+
+    public Date getUpdTimestamp() {
+        return updTimestamp;
+    }
+
+    public void setUpdTimestamp(Date updTimestamp) {
+        this.updTimestamp = updTimestamp;
+    }
+}
