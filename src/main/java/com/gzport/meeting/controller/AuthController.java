3eafdedb89178afe529938bb28c0db2b58f160b2
zhangxiang <624827252@qq.com>
2018-12-02 15:47:38 +0800

    获取用户名字接口

---
+++ src/main/java/com/gzport/meeting/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.gzport.meeting.controller;
+
+import com.gzport.meeting.domain.entity.Auth;
+import com.gzport.meeting.service.AuthService;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhangxiang on 2018/12/2.
+ */
+
+@RestController
+@RequestMapping("/login")
+public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @GetMapping("/getLoginUser")
+    public String getLoginUser(){
+        Auth auth=authService.findByAccount(SecurityUtils.getSubject().getPrincipal().toString());
+        return auth.getName();
+    }
+}
