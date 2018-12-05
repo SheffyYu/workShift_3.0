c5359f24d859e702ae4345015e37b41d0c0c760c
sheffyYu <ysh5835938163@163.com>
2018-12-05 19:06:25 +0800

    2018.12.5 y:修改各个数据录入界面，提交测试未完成

--- src/main/resources/static/js/huadong/form.js
+++ src/main/resources/static/js/huadong/form.js
@@ -1,6 +1,7 @@
 /**
  * Created by yushihui on 2018/12/5.
  */
+document.write("<script language=javascript src='../../js/huadong/tips.js'></script>");
 //最终传递的数据
 var submitJson;
 //散货疏运的数据
@@ -15,6 +16,8 @@ var bargeXS;
 var truck;
 //集装箱堆存
 var cntrStore;
+//判断是否有条件不满足,不满足则为false,不进行操作
+var isContinue;
 
 //初始化数据格式
 function initData() {
@@ -32,6 +35,7 @@ function initData() {
   bargeXS=[];
   truck=[];
   cntrStore=[];
+  isContinue=true;
 }
 
 //待作业=在港艘数-作业
@@ -46,7 +50,38 @@ function work() {
 function proLine() {
   var str1=document.getElementById("shipProLine").value;
   var str2=document.getElementById("bargeProLine").value;
-  document.getElementById("allProLine").value=(str1-0)+(str2-0);
+  document.getElementById("allProLine").value=(str1-0)+(str2-0);//-0是为了避免str拼接
+}
+//集装箱总堆存=重箱内外贸+控箱
+function cntrOper() {
+  var str1=document.getElementById("LIn").value;
+  var str2=document.getElementById("LOut").value;
+  var str3=document.getElementById("Emp").value;
+  document.getElementById("allSto").value=(str1-0)+(str2-0)+(str3-0);
+}
+//车卡港内公司
+function truckCmpOper() {
+  var trkxg=document.getElementById("trkxg").value;
+  var trkxj=document.getElementById("trkxj").value;
+  var trkgct=document.getElementById("trkgct").value;
+  var trkxs=document.getElementById("trkxs").value;
+  document.getElementById("allcmp").value=(trkxg-0)+(trkxj-0)+(trkgct-0)+(trkxs-0);
+}
+//车卡港内货物
+function truckCargoOper() {
+  var trkmt=document.getElementById("trkmt").value;
+  var trkks=document.getElementById("trkks").value;
+  var trkzh=document.getElementById("trkzh").value;
+  document.getElementById("allcgo").value=(trkmt-0)+(trkks-0)+(trkzh-0);
+}
+//目前只有西基需要计算总疏运量
+function xjbarge() {
+  var total=0;
+  $(".dispersion1").each(function () {
+    var str=document.getElementsByClassName("dispersion1").value;
+    total=total+(str-0);
+  });
+  document.getElementById("xjtotal").value=total;
 }
 
 //提交按钮的点击事件
@@ -54,11 +89,29 @@ function submitBtn() {
   //初始化数据
   initData();
 //散货疏运
-  dispersionData();
-  //集装箱驳船
-  bargeData();
-  //集装箱作业线
-  proLineData();
+  if(document.getElementById("dispersion")){
+    dispersionData();
+  }
+  if(document.getElementById("barge")){
+    //集装箱驳船
+    bargeData();
+  }
+  if(document.getElementById("productionLine")){
+    //集装箱作业线
+    proLineData();
+  }
+  if(document.getElementById("cntrSto")){
+    //集装箱堆存
+    cntrStoreData();
+  }
+  if(document.getElementById("truck")){
+    //车卡
+    truckData();
+  }
+  if(document.getElementById("xsCntr")){
+    //新沙集装箱
+    xsCntrData();
+  }
 
   //整合数据
   submitJson.dispersionVOList=disper;
@@ -71,7 +124,9 @@ function submitBtn() {
   console.log(submitJson);
 
   //提交数据
-  postJson();
+  if(isContinue){
+    postJson();
+  }
 }
 
 //提交数据
@@ -84,7 +139,10 @@ function postJson() {
     type: "POST",
     data:JSON.stringify(submitJson),
     success: function (data) {
-      console.log("提交成功！");
+      //刷新窗口
+      // window.location.reload();
+      showTips("提交成功！",200,0.5);
+      // setTimeout(location.reload(), 1000 );
     }
   });
 }
@@ -126,15 +184,31 @@ function dispersionData() {
     i++;
   });
   for(var k=0;i>k;k++){
-    var obj={
-      dispersionId:"",
-      terCode:"",
-      cargoName:name[k],
-      workingNumber:dis1[k],
-      unWorkNumber:dis2[k],
-      mechanicalNumber:dis3[k]
-    };
-    disper.push(obj);
+    if(!(dis1[k] == "" && dis2[k] == "" && dis2[k] == "") ){
+      if(dis1[k] == ""){
+        dis1[k]=0;
+      }
+      if(dis2[k] == ""){
+        dis2[k]=0;
+      }
+      if(dis3[k] == ""){
+        dis3[k]=0;
+      }
+      if(name[k] == ""){
+        isContinue=false;
+        alert("请输入货类名！");
+      }else{
+        var obj={
+          dispersionId:"",
+          terCode:"",
+          cargoName:name[k],
+          workingNumber:dis1[k],
+          unWorkNumber:dis2[k],
+          mechanicalNumber:dis3[k]
+        };
+        disper.push(obj);
+      }
+    }
   }
   //以上是疏运的内容
   console.log(disper);
@@ -145,15 +219,22 @@ function bargeData(){
   var inPort=document.getElementById("inPort").value;
   var finishWork=document.getElementById("finishWork").value;
   var unWork=document.getElementById("unWork").value;
-  var obj={
-    bargeId:"",		//驳船Id，没有可以不填
-    terCode:"",		//码头代码，不用填
-    shipNumber:inPort,		//船数量
-    workingNumber:finishWork,	//作业数量
-    unWorkNumber:unWork		//待作业数量
-  };
-  barge.push(obj);
-  console.log(barge);
+
+  //对取到的数据进行判断,不能为空
+  if(inPort == "" || finishWork == "" || unWork ==""){
+    isContinue=false;
+    alert("集装箱驳船中数据不能为空，请返回填写！");//返回界面
+  }else{
+    var obj={
+      bargeId:"",		//驳船Id，没有可以不填
+      terCode:"",		//码头代码，不用填
+      shipNumber:inPort,		//船数量
+      workingNumber:finishWork,	//作业数量
+      unWorkNumber:unWork		//待作业数量
+    };
+    barge.push(obj);
+    console.log(barge);
+  }
 }
 
 //集装箱作业线的数据处理
@@ -161,13 +242,144 @@ function proLineData() {
   var allLine=document.getElementById("allProLine").value;
   var shipLine=document.getElementById("shipProLine").value;
   var bargeLine=document.getElementById("bargeProLine").value;
-  var obj={
-    productionId:"",	//作业线Id,没有可以不填
-    terCode:"",		//码头代码，不用填
-    totalLine:allLine,		//总作业线
-    shipLine:shipLine,		//大船作业线
-    bargeLine:bargeLine		//驳船作业线
-  };
-  proroductionLine.push(obj);
-  console.log(proroductionLine);
+  if(allLine == "" || shipLine == "" || bargeLine == ""){
+    isContinue=false;
+    alert("集装箱作业线中数据不能为空，请返回填写！");//返回界面
+  } else{
+    var obj={
+      productionId:"",	//作业线Id,没有可以不填
+      terCode:"",		//码头代码，不用填
+      totalLine:allLine,		//总作业线
+      shipLine:shipLine,		//大船作业线
+      bargeLine:bargeLine		//驳船作业线
+    };
+    proroductionLine.push(obj);
+    console.log(proroductionLine);
+  }
+}
+
+//集装箱堆存的数据处理
+function cntrStoreData() {
+  var LIn=document.getElementById("LIn").value;
+  var LOut=document.getElementById("LOut").value;
+  var Emp=document.getElementById("Emp").value;
+  var allSto=document.getElementById("allSto").value;
+  if(LIn == "" || LOut == "" || Emp == "" || allSto == ""){
+    isContinue=false;
+    alert("集装箱堆存中数据不能为空，请返回填写！");
+  }else {
+    var obj={
+      cntrStoreId:"",
+      terCode:"",
+      empCntr:Emp,
+      loadCntrE:LOut,
+      loadCntrI:LIn,
+      totalCntr:allSto
+    };
+    cntrStore.push(obj);
+    console.log(cntrStore);
+  }
+}
+
+//车卡
+function truckData() {
+  var trkxg=document.getElementById("trkxg").value;
+  var trkxj=document.getElementById("trkxj").value;
+  var trkgct=document.getElementById("trkgct").value;
+  var trkxs=document.getElementById("trkxs").value;
+  var allcmp=document.getElementById("allcmp").value;
+  var trkmt=document.getElementById("trkmt").value;
+  var trkks=document.getElementById("trkks").value;
+  var trkzh=document.getElementById("trkzh").value;
+  var allcgo=document.getElementById("allcgo").value;
+  var trkmte=document.getElementById("trkmte").value;
+  var trkkse=document.getElementById("trkkse").value;
+  var alle=document.getElementById("alle").value;
+  var trkload=document.getElementById("trkload").value;
+  var trkunload=document.getElementById("trkunload").value;
+  var trkothere=document.getElementById("trkothere").value;
+
+  //当港内货物总和与公司不相等时
+  if(allcmp != allcgo){
+    isContinue=false;
+    alert("港内公司与港内货物合计不相等，请返回检查并修改！");
+  }else{
+    var obj={
+      truckStoreId:"",
+      loadInport:allcmp,
+      loadOutport:alle,
+      loadiCoal:trkmt,
+      loadiOre:trkks,
+      loadiOther:trkzh,
+      loadiGct:trkgct,
+      loadiXg:trkxg,
+      loadiXj:trkxj,
+      loadiXs:trkxs,
+      loadoCoal:trkmte,
+      loadoOre:trkkse,
+      loadoOther:trkothere,
+      totalLoad:(allcmp-0)+(alle-0),
+      unloadTruck:trkload,
+      waitUnload:trkunload,
+      terCode:""
+    };
+    truck.push(obj);
+    console.log(truck);
+  }
+}
+
+//新沙集装箱码头的数据处理
+function xsCntrData() {
+var xstitle=[],xsShen=[],xsI=[],xsE=[],xsBus=[],xstotal=[];
+var i=0;
+$("#xsCntr .kv-item span").each(function () {
+  var node=$(this).attr("title");
+    xstitle[i]=node;
+    i++;
+});
+i=0;
+$("#xsCntr .xsShen").each(function () {
+  var node=$(this).value;
+  xsShen[i]=node;
+  i++;
+});
+  i=0;
+  $("#xsCntr .xsI").each(function () {
+    var node=$(this).value;
+    xsI[i]=node;
+    i++;
+  });
+  i=0;
+  $("#xsCntr .xsE").each(function () {
+    var node=$(this).value;
+    xsE[i]=node;
+    i++;
+  });
+  i=0;
+  $("#xsCntr .xsBus").each(function () {
+    var node=$(this).value;
+    xsBus[i]=node;
+    i++;
+  });
+  i=0;
+  $("#xsCntr .xstotal").each(function () {
+    var node=$(this).value;
+    xstotal[i]=node;
+    i++;
+  });
+  for(var k=0;k<i;k++){
+    var obj={
+      bargeXSId:"",		//新沙驳船Id,没有可不填
+      terCode:"",		//码头代码，不用填
+      shipNumber:"",		//同barge
+      workingNumber:"",	//同barge
+      unWorkNumber:"",	//同barge
+      shuttlebusNumber:xsBus[k],	//穿巴数
+      bargeItNumber:xsI[k],	//深圳内贸
+      bargeShenzhen:xsShen[k],
+      bargeEtNumber:xsE[k]	//外贸
+    };
+    bargeXS.push(obj);
+  }
+  console.log(bargeXS);
 }
---
+++ src/main/resources/static/js/huadong/tips.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yushihui on 2018/12/5.
+ */
+function showTips( content, height, time ){
+  //窗口的宽度
+  var windowWidth  = $(window).width();
+  var tipsDiv = '<div class="tipsClass">' + content + '</div>';
+
+  $( 'body' ).append( tipsDiv );
+  $( 'div.tipsClass' ).css({
+    'top'       : height + 'px',
+    'left'      : ( windowWidth / 2 ) - 350/2 + 'px',
+    'position'  : 'absolute',
+    'padding'   : '40px 5px',
+    'background': '#0e8bc5',
+    'font-size' : 20+ 'px',
+    'margin'    : '0 auto',
+    'text-align': 'center',
+    'width'     : '350px',
+    'height'    : '60px',
+    'color'     : '#fff',
+    'opacity'   : '0.8'
+  }).show();
+  setTimeout( function(){$( 'div.tipsClass' ).fadeOut();}, ( time * 1000 ) );
+}
