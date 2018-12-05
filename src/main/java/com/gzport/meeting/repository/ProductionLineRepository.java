fd76b340346fbb1a371623f651b1b88c5e1c8991
zhangxiang <624827252@qq.com>
2018-12-05 10:47:03 +0800

    BargeXS修改字段

--- src/main/java/com/gzport/meeting/repository/ProductionLineRepository.java
+++ src/main/java/com/gzport/meeting/repository/ProductionLineRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
  */
 public interface ProductionLineRepository extends PagingAndSortingRepository<ProductionLine,String> {
     @Modifying
-    @Query(value = "SELECT * FROM PRODUCTION_LINE  WHERE trunc(INS_TIMESTAMP)＝to_date(?1,'yyyy-mm-dd') AND TER_CODE=?2;",nativeQuery = true)
+    @Query(value = "SELECT * FROM PRODUCTION_LINE  WHERE trunc(INS_TIMESTAMP)＝to_date(?1,'yyyy-mm-dd') AND TER_CODE=?2",nativeQuery = true)
     public List<ProductionLine> findCurrentDataByWharf(String date, String terId);
 }
