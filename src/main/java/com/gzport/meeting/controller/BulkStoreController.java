064041d8d5c7f1f5177a99a350b2d70b241c0d30
zhangxiang <624827252@qq.com>
2018-12-10 11:41:28 +0800

    bulk获取当天数据

--- src/main/java/com/gzport/meeting/controller/BulkStoreController.java
+++ src/main/java/com/gzport/meeting/controller/BulkStoreController.java
@@ -1,7 +1,29 @@
 package com.gzport.meeting.controller;
 
+import com.gzport.meeting.domain.entity.BulkStore;
+import com.gzport.meeting.service.BulkStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by zhangxiang on 2018/12/10.
  */
-public class BulkStoreController {
+@RequestMapping("/login")
+@RestController
+public class BulkStoreController  {
+
+    @Autowired
+    BulkStoreService bulkStoreService;
+
+    @GetMapping("/getBulk/Daily")
+    public List<BulkStore> findCurrentStoreByDate(){
+        String date=new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        return bulkStoreService.getBulkByTime(date);
+    }
 }
