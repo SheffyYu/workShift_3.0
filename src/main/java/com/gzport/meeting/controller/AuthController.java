ee9882deee7883981a5476aa6cb0f90c003afe75
zhangxiang <624827252@qq.com>
2018-12-12 09:10:16 +0800

    修改密码接口

--- src/main/java/com/gzport/meeting/controller/AuthController.java
+++ src/main/java/com/gzport/meeting/controller/AuthController.java
@@ -4,11 +4,10 @@ import com.gzport.meeting.domain.AuthConverter;
 import com.gzport.meeting.domain.dto.AuthInfo;
 import com.gzport.meeting.domain.entity.Auth;
 import com.gzport.meeting.service.AuthService;
+import net.huadong.idev.ezui.utils.HdCipher;
 import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by zhangxiang on 2018/12/2.
@@ -28,5 +27,15 @@ public class AuthController {
     public AuthInfo getLoginUser(){
         Auth auth=authService.findByAccount(SecurityUtils.getSubject().getPrincipal().toString());
         return authConverter.convert(auth);
+    }
+
+    @PostMapping("/changePassword")
+    public AuthInfo changePassowrd(@RequestParam("password") String password){
+        Auth auth=authService.findByAccount(SecurityUtils.getSubject().getPrincipal().toString());
+        auth.setPassword(HdCipher.getMD(password, "MD5"));
+        if(authService.createAuth(auth)!=null){
+            return authConverter.convert(auth);
+        }else
+            return null;
     }
 }
