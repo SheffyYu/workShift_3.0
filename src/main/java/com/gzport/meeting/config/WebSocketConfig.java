6744383a62bc582658faf7c6f842d10439383dfb
zhangxiang <624827252@qq.com>
2019-01-03 16:12:56 +0800

    websocketconfig

---
+++ src/main/java/com/gzport/meeting/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.gzport.meeting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * Created by zhangxiang on 2019/1/3.
+ */
+@Configuration
+public class WebSocketConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+
+}
