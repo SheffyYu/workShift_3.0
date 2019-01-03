9eaca5a9282e2dda7ac02602bc6cc914f436927b
sheffyYu <ysh5835938163@163.com>
2019-01-03 17:03:17 +0800

    2019.1.3 y:实时推送申请修改数据

--- src/main/resources/static/js/huadong/tips.js
+++ src/main/resources/static/js/huadong/tips.js
@@ -23,3 +23,44 @@ function showTips( content, height, time ){
   }).show();
   setTimeout( function(){$( 'div.tipsClass' ).fadeOut();}, ( time * 1000 ) );
 }
+
+/**
+ * 初始化弹出框
+ * @param title
+ * @param content
+ */
+function ontime_pop_init(title,content) {
+  //取当前浏览器窗口大小
+  var windowWidth=$(document).width();
+  var windowHeight=$(document).height();
+  //弹窗的大小
+  var weight=320;
+  var height=240;
+  $("body").append(
+    "<div id='ontime_pop_div'style='display:none;position:absolute;border:1px solid #e0e0e0;z-index:99;width:"+weight+"px;height:"+height+"px;top:"+(windowHeight-height-10)+"px;left:"+(windowWidth-weight-10)+"px'>"+
+    "<div style='line-height:32px;background:#f6f0f3;border-bottom:1px solid #e0e0e0;font-size:14px;padding:0 0 0 10px;'>" +
+    "<div style='float:left;'><b>"+title+"</b></div><div style='float:right;cursor:pointer;'><b onclick='ontime_pop_close()'>×</b></div>" +
+    "<div style='clear:both'></div>"+
+    "</div>" +
+    "<div id='ontime_content'>" +
+    content+
+    "</div>"+
+    "</div>"
+  );
+}
+/**
+ * 关闭弹出框
+ */
+function ontime_pop_close(){
+  $('#ontime_pop_div').fadeOut(400);
+}
+
+
+/**
+ * 实时推送消息
+ */
+function showOnTime(msg){
+  var str="<ul><li>"+msg+"</li></ul>";
+  ontime_pop_init("公告信息",str);
+  $('#ontime_pop_div').fadeIn(400);
+}
