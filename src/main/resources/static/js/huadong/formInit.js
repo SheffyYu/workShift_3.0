d6c3fe2fbcc21e08df3a60110fa7462d484ce772
sheffyYu <ysh5835938163@163.com>
2019-01-09 16:38:58 +0800

    2019.1.9 y：解决车卡港外总计undefined的问题

--- src/main/resources/static/js/huadong/formInit.js
+++ src/main/resources/static/js/huadong/formInit.js
@@ -222,10 +222,11 @@ function insertTruckData() {
   document.getElementById("trkmte").value=truck[0].loadoCoal;
   document.getElementById("trkkse").value=truck[0].loadoOre;
   document.getElementById("trkothere").value=truck[0].loadoOther;
-  document.getElementById("alle").value=truck[0].loadOutport;
+  document.getElementById("alle").value=truck[0].loadOutPort;
   document.getElementById("trkload").value=truck[0].unloadTruck;
   document.getElementById("trkunload").value=truck[0].waitUnload;
   document.getElementById("totalLoad").value=truck[0].totalLoad;
+  // console.log(truck[0]);
 }
 
 /**
