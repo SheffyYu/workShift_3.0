9bdbf71aaba67d217cc0d3c5d98416df1f1e58b4
sheffyYu <ysh5835938163@163.com>
2019-02-12 16:45:48 +0800

    2019.2.12 y:船舶调度计划大屏接口

---
+++ src/main/java/com/gzport/meeting/domain/vo/ShipRealtmVO.java
@@ -0,0 +1,243 @@
+package com.gzport.meeting.domain.vo;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by yushihui on 2019/2/12.
+ */
+public class ShipRealtmVO {
+
+    private String bertreal;
+
+    private String vslname;
+
+    private String vbt_comejob;
+
+    private String gmtname;
+
+    private String sumnum;
+
+    private String vwkastarttm;
+
+    private String berthtime;
+
+    private String leavetime;
+
+    private String portcomp;
+
+    private String effect;
+
+    private String workpace;
+
+    private String iscash;
+
+    private String vbt_pberthtm;
+
+    private String vbt_pleavetm;
+
+    public ShipRealtmVO(){}
+
+    public ShipRealtmVO(String bertreal, String vslname, String vbt_comejob, String gmtname, String sumnumi, String sumnume, Date vwkastarttm, Date berthtime, Date leavetime, String portcomp, String effect, Float workpace, String iscash, Date vbt_pberthtm, Date vbt_pleavetm){
+        if(bertreal != null){
+            this.bertreal=bertreal;
+        }else{
+            this.bertreal="";
+        }
+        if (vslname != null){
+            this.vslname=vslname;
+        }else{
+            this.vslname="";
+        }
+        if (vbt_comejob != null){
+            this.vbt_comejob=vbt_comejob;
+        }else {
+            this.vbt_comejob="";
+        }
+        if(gmtname!=null){
+            this.gmtname=gmtname;
+        }else {
+            this.gmtname="";
+        }
+        if (vbt_comejob == "卸"){
+            if(sumnumi != null){
+                this.sumnum=sumnumi;
+            }else{
+                this.sumnum="";
+            }
+        }else if (vbt_comejob == "装"){
+            if(sumnume != null){
+                this.sumnum=sumnume;
+            }else{
+                this.sumnum="";
+            }
+        }else {
+            if(sumnume == null){
+                sumnume="";
+            }
+            if(sumnumi == null){
+                sumnumi="";
+            }
+            this.sumnum=sumnumi+'/'+sumnume;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/hh24:mm");
+        if (vwkastarttm != null){
+            this.vwkastarttm=sdf.format(vwkastarttm);
+        }else {
+            this.vwkastarttm="";
+        }
+        if (berthtime != null){
+            this.berthtime=sdf.format(berthtime);
+        }else {
+            this.berthtime="";
+        }
+        if (leavetime != null){
+            this.leavetime=sdf.format(leavetime);
+        }else {
+            this.leavetime="";
+        }
+        this.portcomp=portcomp;
+        DecimalFormat fnum = new DecimalFormat("##0.00");
+        if(effect != null){
+            this.effect=fnum.format(Float.parseFloat(effect));
+        }else{
+            this.effect="";
+        }
+        if(workpace != null){
+            this.workpace=fnum.format(workpace*100)+"%";
+        }else{
+            this.workpace="";
+        }
+
+        if (iscash == "Y"){
+            this.iscash="是";
+        }else if(iscash == "N"){
+            this.iscash="否";
+        }else {
+            this.iscash="";
+        }
+        if(vbt_pberthtm != null ){
+            this.vbt_pberthtm=sdf.format(vbt_pberthtm);
+        }else {
+            this.vbt_pberthtm="";
+        }
+        if(vbt_pleavetm != null ){
+            this.vbt_pleavetm=sdf.format(vbt_pleavetm);
+        }else {
+            this.vbt_pleavetm="";
+        }
+    }
+
+    public String getBertreal() {
+        return bertreal;
+    }
+
+    public void setBertreal(String bertreal) {
+        this.bertreal = bertreal;
+    }
+
+    public String getVslname() {
+        return vslname;
+    }
+
+    public void setVslname(String vslname) {
+        this.vslname = vslname;
+    }
+
+    public String getVbt_comejob() {
+        return vbt_comejob;
+    }
+
+    public void setVbt_comejob(String vbt_comejob) {
+        this.vbt_comejob = vbt_comejob;
+    }
+
+    public String getGmtname() {
+        return gmtname;
+    }
+
+    public void setGmtname(String gmtname) {
+        this.gmtname = gmtname;
+    }
+
+    public String getSumnum() {
+        return sumnum;
+    }
+
+    public void setSumnum(String sumnum) {
+        this.sumnum = sumnum;
+    }
+
+    public String getVwkastarttm() {
+        return vwkastarttm;
+    }
+
+    public void setVwkastarttm(String vwkastarttm) {
+        this.vwkastarttm = vwkastarttm;
+    }
+
+    public String getBerthtime() {
+        return berthtime;
+    }
+
+    public void setBerthtime(String berthtime) {
+        this.berthtime = berthtime;
+    }
+
+    public String getLeavetime() {
+        return leavetime;
+    }
+
+    public void setLeavetime(String leavetime) {
+        this.leavetime = leavetime;
+    }
+
+    public String getPortcomp() {
+        return portcomp;
+    }
+
+    public void setPortcomp(String portcomp) {
+        this.portcomp = portcomp;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public void setEffect(String effect) {
+        this.effect = effect;
+    }
+
+    public String getWorkpace() {
+        return workpace;
+    }
+
+    public void setWorkpace(String workpace) {
+        this.workpace = workpace;
+    }
+
+    public String getIscash() {
+        return iscash;
+    }
+
+    public void setIscash(String iscash) {
+        this.iscash = iscash;
+    }
+
+    public String getVbt_pberthtm() {
+        return vbt_pberthtm;
+    }
+
+    public void setVbt_pberthtm(String vbt_pberthtm) {
+        this.vbt_pberthtm = vbt_pberthtm;
+    }
+
+    public String getVbt_pleavetm() {
+        return vbt_pleavetm;
+    }
+
+    public void setVbt_pleavetm(String vbt_pleavetm) {
+        this.vbt_pleavetm = vbt_pleavetm;
+    }
+}
---
+++ src/main/java/com/gzport/meeting/screen/controller/ScreenShipRealtmController.java
@@ -0,0 +1,45 @@
+package com.gzport.meeting.screen.controller;
+
+import com.gzport.meeting.domain.entity.VesselShipRealtim;
+import com.gzport.meeting.domain.entity.Vessels;
+import com.gzport.meeting.domain.vo.ShipRealtmVO;
+import com.gzport.meeting.service.ShipRealtmService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by yushihui on 2019/2/12.
+ */
+@RestController
+@RequestMapping("/screen/shipRealtm")
+@Api(description = "船舶调度计划大屏接口")
+public class ScreenShipRealtmController {
+    @Autowired
+    ShipRealtmService shipRealtmService;
+
+    @GetMapping("/getShipRealtm")
+    @CrossOrigin("http://datav.aliyun.com")
+    @ApiOperation(value = "所有船舶调度计划大屏接口")
+    public List<ShipRealtmVO> getAllData(String portcomp){
+        List<ShipRealtmVO> srvoList=new ArrayList<>();
+        List<VesselShipRealtim> vsrList=new ArrayList<>();
+        String[] terCode=portcomp.split(",");
+        for (int i=0;i<terCode.length;i++){
+            vsrList.addAll(shipRealtmService.findAll(terCode[i]));
+        }
+        Collections.reverse(vsrList);
+        for (VesselShipRealtim vsr : vsrList){
+            srvoList.add(new ShipRealtmVO(vsr.getBertreal(),vsr.getVslname(),vsr.getVbt_comejob(),vsr.getGmtname(),vsr.getSumnumi(),vsr.getSumnume(),vsr.getVwkastarttm(),vsr.getBerthtime(),vsr.getLeavetime(),vsr.getPortcomp(),vsr.getEffect(),vsr.getWorkpace(),vsr.getIscash(),vsr.getVbt_pberthtm(),vsr.getVbt_pleavetm()));
+        }
+        return srvoList;
+    }
+}
