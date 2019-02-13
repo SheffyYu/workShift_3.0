9fdf802f678fa589455992ebf27bca26e9ae9a17
zhangxiang <624827252@qq.com>
2019-02-13 16:54:54 +0800

    月度集装箱吞吐量status交换

---
+++ src/main/java/com/gzport/meeting/config/WebMvcConfiguration.java
@@ -0,0 +1,22 @@
+package com.gzport.meeting.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by zhangxiang on 2019/2/13.
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers")
+                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+                .allowCredentials(true).maxAge(3600);;
+    }
+}
